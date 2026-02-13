8ddde6e950c4b2395dc5a41239525be7db41e9e8
Sasha <sasha.ryabokon14@gmail.com>
2026-02-13 14:29:59 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -29,9 +29,7 @@ def create_movie(
         description=movie_description,
     )
     if genres_ids:
-        genres = Genre.objects.filter(id__in=genres_ids)
-        movie.genres.set(genres)
+        movie.genres.set(genres_ids)
     if actors_ids:
-        actors = Actor.objects.filter(id__in=actors_ids)
-        movie.actors.set(actors)
+        movie.actors.set(actors_ids)
     return movie
